2190b3ba93715d7ebee45b9784fd524d48acad03
Chris McKnight <tenlongfingers10@gmail.com>
2022-06-25 11:15:38 -0600

    still struggling with checking to see if a div is empty

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -1,5 +1,6 @@
 
 import { Builder, Capabilities, By } from "selenium-webdriver"
+import { forEachChild, visitEachChild } from "typescript"
 
 require('chromedriver')
 
@@ -17,9 +18,17 @@ test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
+
 })
-// test('Title shows up when page loads', async () => {
-//     const title = await driver.findElement(By.id('title'))
-//     const displayed = await title.isDisplayed()
-//     expect(displayed).toBe(true)
-// })
+test('Draw button displays choices', async () => {
+    await driver.findElement(By.id('draw')).click();
+    const choices = await driver.findElement(By.id('choices'))
+    expect(choices.isDisplayed()).toBeTruthy
+})
+//This one I expect to return false
+test('Show all robots button works', async () =>{
+    await driver.findElement(By.id('see-all')).click();
+    const allBots = await driver.findElement(By.id('all-bots'))
+    const botCard = await driver.findElement(By.className('bot-card'))
+    expect(allBots).toContain(botCard);
+})
